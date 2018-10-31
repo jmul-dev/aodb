25aeb70a8061412aca0204520c1b7f044ab2e82e
Johan <johan@paramation.com>
2018-10-31 13:18:19 +0700

    Fix test/collisions.js

--- test/collisions.js
+++ test/collisions.js
@@ -1,65 +1,72 @@
 var tape = require('tape')
 var create = require('./helpers/create')
+var EthCrypto = require('eth-crypto');
+var identity = EthCrypto.createIdentity();
+
+var key1 = '/' + identity.publicKey + '/idgcmnmna';
+var value1 = 'a';
+var key2 = '/' + identity.publicKey + '/mpomeiehc';
+var value2 = 'b';
 
 tape('two keys with same siphash', function (t) {
-  t.plan(2 + 2)
+	t.plan(2 + 2)
 
-  var db = create.one()
+	var db = create.one()
 
-  db.put('idgcmnmna', 'a', function () {
-    db.put('mpomeiehc', 'b', function () {
-      db.get('idgcmnmna', function (err, node) {
-        t.error(err, 'no error')
-        t.same(node.value, 'a')
-      })
-      db.get('mpomeiehc', function (err, node) {
-        t.error(err, 'no error')
-        t.same(node.value, 'b')
-      })
-    })
-  })
+	db.put(key1, value1, EthCrypto.sign(identity.privateKey, db.createSignHash(key1, value1)), identity.publicKey, function () {
+		db.put(key2, value2, EthCrypto.sign(identity.privateKey, db.createSignHash(key2, value2)), identity.publicKey, function () {
+			db.get(key1, function (err, node) {
+				t.error(err, 'no error')
+				t.same(node.value, value1)
+			})
+			db.get(key2, function (err, node) {
+				t.error(err, 'no error')
+				t.same(node.value, value2)
+			})
+		})
+	})
 })
 
 tape('two keys with same siphash (iterator)', function (t) {
-  var db = create.one()
+	var db = create.one()
 
-  db.put('idgcmnmna', 'a', function () {
-    db.put('mpomeiehc', 'b', function () {
-      var ite = db.iterator()
+	db.put(key1, value1, EthCrypto.sign(identity.privateKey, db.createSignHash(key1, value1)), identity.publicKey, function () {
+		db.put(key2, value2, EthCrypto.sign(identity.privateKey, db.createSignHash(key2, value2)), identity.publicKey, function () {
+			var ite = db.iterator()
 
-      ite.next(function (err, node) {
-        t.error(err, 'no error')
-        t.same(node.value, 'a')
-      })
-      ite.next(function (err, node) {
-        t.error(err, 'no error')
-        t.same(node.value, 'b')
-      })
-      ite.next(function (err, node) {
-        t.error(err, 'no error')
-        t.same(node, null)
-        t.end()
-      })
-    })
-  })
+			ite.next(function (err, node) {
+				t.error(err, 'no error')
+				t.same(node.value, value1)
+			})
+			ite.next(function (err, node) {
+				t.error(err, 'no error')
+				t.same(node.value, value2)
+			})
+			ite.next(function (err, node) {
+				t.error(err, 'no error')
+				t.same(node, null)
+				t.end()
+			})
+		})
+	})
 })
 
 tape('two prefixes with same siphash (iterator)', function (t) {
-  var db = create.one()
+	var db = create.one()
 
-  db.put('idgcmnmna/a', 'a', function () {
-    db.put('mpomeiehc/b', 'b', function () {
-      var ite = db.iterator('idgcmnmna')
+	db.put(key1 + '/a', value1, EthCrypto.sign(identity.privateKey, db.createSignHash(key1 + '/a', value1)), identity.publicKey, function () {
+		db.put(key2 + '/b', value2, EthCrypto.sign(identity.privateKey, db.createSignHash(key2 + '/b', value2)), identity.publicKey, function () {
+			var ite = db.iterator(key1)
 
-      ite.next(function (err, node) {
-        t.error(err, 'no error')
-        t.same(node.value, 'a')
-      })
-      ite.next(function (err, node) {
-        t.error(err, 'no error')
-        t.same(node, null)
-        t.end()
-      })
-    })
-  })
+			ite.next(function (err, node) {
+				t.error(err, 'no error')
+				t.same(node.value, value1)
+			})
+			ite.next(function (err, node) {
+				t.error(err, 'no error')
+				t.same(node, null)
+				t.end()
+			})
+		})
+	})
 })
