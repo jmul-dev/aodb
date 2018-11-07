06d36cf874426b5f1ef477f1026a121905e95bf8
Johan <johan@paramation.com>
2018-11-07 17:29:40 +0700

    Fix test/iterator-order.js

--- test/helpers/put.js
+++ test/helpers/put.js
@@ -1,17 +1,21 @@
-var EthCrypto = require('eth-crypto');
+var EthCrypto = require("eth-crypto");
 
-module.exports = function (db, privateKey, writerAddress, list, opts, cb) {
-	var i = 0
-	loop(null)
+module.exports = function(db, privateKey, writerAddress, list, opts, cb) {
+	var i = 0;
+	loop(null);
 
-	function loop (err) {
-		if (err) return cb(err)
-		if (i === list.length) return cb(null)
+	function loop(err) {
+		if (err) return cb(err);
+		if (i === list.length) return cb(null);
 
-		var next = list[i++]
-		if (typeof next === 'string') next = {key: next, value: next}
+		var next = list[i++];
+		if (typeof next === "string") next = { key: next, value: next };
+		if (typeof next === "object" && next.schemaKey) {
+			opts.schemaKey = next.schemaKey;
+			next = { key: next.key, value: next.key };
+		}
 
 		var signature = EthCrypto.sign(privateKey, db.createSignHash(next.key, next.value));
-		db.put(next.key, next.value, signature, writerAddress, opts, loop)
+		db.put(next.key, next.value, signature, writerAddress, opts, loop);
 	}
-}
+};
--- test/iterator-order.js
+++ test/iterator-order.js
@@ -1,131 +1,299 @@
-var tape = require('tape')
-var cmp = require('compare')
-var create = require('./helpers/create')
-var put = require('./helpers/put')
-var run = require('./helpers/run')
-var hash = require('../lib/hash')
+const tape = require("tape");
+const cmp = require("compare");
+const create = require("./helpers/create");
+const put = require("./helpers/put");
+const run = require("./helpers/run");
+const hash = require("../lib/hash");
+const EthCrypto = require("eth-crypto");
+const { privateKey, publicKey: writerAddress } = EthCrypto.createIdentity();
 
-function sortByHash (a, b) {
-  var ha = hash(typeof a === 'string' ? a : a.key).join('')
-  var hb = hash(typeof b === 'string' ? b : b.key).join('')
-  return cmp(ha, hb)
-}
+const sortByHash = (a, b) => {
+	const ha = hash(typeof a === "string" ? a : a.key).join("");
+	const hb = hash(typeof b === "string" ? b : b.key).join("");
+	return cmp(ha, hb);
+};
 
-function reverseSortByHash (a, b) {
-  return -1 * sortByHash(a, b)
-}
+const reverseSortByHash = (a, b) => {
+	return -1 * sortByHash(a, b);
+};
+
+const schemaKey = "schema/*";
+const schemaValue = {
+	keySchema: "*",
+	valueValidationKey: "",
+	keyValidation: ""
+};
+
+const schemaKey2 = "schema/*/*";
+const schemaValue2 = {
+	keySchema: "*/*",
+	valueValidationKey: "",
+	keyValidation: ""
+};
+
+const schemaKey3 = "schema/*/*/*";
+const schemaValue3 = {
+	keySchema: "*/*/*",
+	valueValidationKey: "",
+	keyValidation: ""
+};
+
+const testSingleFeedWithKeys = (t, keys, opts, cb) => {
+	if (typeof opts === "function") return testSingleFeedWithKeys(t, keys, null, opts);
+	opts = opts || {};
+
+	const sortFunc = opts.sort || sortByHash;
+
+	t.comment("with single feed");
+	const db = create.one();
+	const batchList = [
+		{
+			type: "add-schema",
+			key: schemaKey,
+			value: schemaValue,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey, schemaValue)),
+			writerAddress: writerAddress
+		},
+		{
+			type: "add-schema",
+			key: schemaKey2,
+			value: schemaValue2,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey2, schemaValue2)),
+			writerAddress: writerAddress
+		},
+		{
+			type: "add-schema",
+			key: schemaKey3,
+			value: schemaValue3,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey3, schemaValue3)),
+			writerAddress: writerAddress
+		}
+	];
+	db.batch(batchList, (err) => {
+		t.error(err, "no error");
+		put(db, privateKey, writerAddress, keys, {}, (err) => {
+			t.error(err, "no error");
+
+			testIteratorOrder(t, db.iterator(opts), keys, sortFunc, cb);
+		});
+	});
+};
+
+const testTwoFeedsWithKeys = (t, keys, opts, cb) => {
+	if (typeof opts === "function") return testTwoFeedsWithKeys(t, keys, null, opts);
+	opts = opts || {};
+
+	const sortFunc = opts.sort || sortByHash;
+
+	t.comment("with values split across two feeds");
+	create.two((db1, db2, replicate) => {
+		const batchList1 = [
+			{
+				type: "add-schema",
+				key: schemaKey,
+				value: schemaValue,
+				writerSignature: EthCrypto.sign(privateKey, db1.createSignHash(schemaKey, schemaValue)),
+				writerAddress: writerAddress
+			},
+			{
+				type: "add-schema",
+				key: schemaKey2,
+				value: schemaValue2,
+				writerSignature: EthCrypto.sign(privateKey, db2.createSignHash(schemaKey2, schemaValue2)),
+				writerAddress: writerAddress
+			},
+			{
+				type: "add-schema",
+				key: schemaKey3,
+				value: schemaValue3,
+				writerSignature: EthCrypto.sign(privateKey, db1.createSignHash(schemaKey3, schemaValue3)),
+				writerAddress: writerAddress
+			}
+		];
+
+		db1.batch(batchList1, (err) => {
+			t.error(err, "no error");
+
+			const batchList2 = [
+				{
+					type: "add-schema",
+					key: schemaKey,
+					value: schemaValue,
+					writerSignature: EthCrypto.sign(privateKey, db2.createSignHash(schemaKey, schemaValue)),
+					writerAddress: writerAddress
+				},
+				{
+					type: "add-schema",
+					key: schemaKey2,
+					value: schemaValue2,
+					writerSignature: EthCrypto.sign(privateKey, db2.createSignHash(schemaKey2, schemaValue2)),
+					writerAddress: writerAddress
+				},
+				{
+					type: "add-schema",
+					key: schemaKey3,
+					value: schemaValue3,
+					writerSignature: EthCrypto.sign(privateKey, db2.createSignHash(schemaKey3, schemaValue3)),
+					writerAddress: writerAddress
+				}
+			];
+
+			db2.batch(batchList2, (err) => {
+				t.error(err, "no error");
+
+				const half = Math.floor(keys.length / 2);
+				const done = () => {
+					if (!cb) t.end();
+					else cb();
+				};
+				run(
+					(cb) => put(db1, privateKey, writerAddress, keys.slice(0, half), {}, cb),
+					(cb) => put(db2, privateKey, writerAddress, keys.slice(half), {}, cb),
+					(cb) => replicate(cb),
+					(cb) => testIteratorOrder(t, db1.iterator(opts), keys, sortFunc, cb),
+					(cb) => testIteratorOrder(t, db2.iterator(opts), keys, sortFunc, cb),
+					done
+				);
+			});
+		});
+	});
+};
+
+const testIteratorOrder = (t, iterator, expected, sortFunc, done) => {
+	const sorted = expected.slice(0).sort(sortFunc);
+	const onEach = (err, node) => {
+		t.error(err, "no error");
+		if (node.length) node = node[0];
+		if (!node.isSchema) {
+			const key = node.key;
+			const expectedNode = sorted.shift();
+			t.same(key, expectedNode.key);
+		}
+	};
+	const onDone = () => {
+		t.same(sorted.length, 0);
+		if (done === undefined) t.end();
+		else done();
+	};
+	each(iterator, onEach, onDone);
+};
+
+const each = (ite, cb, done) => {
+	ite.next(function loop(err, node) {
+		if (err) return cb(err);
+		if (!node) return done();
+		cb(null, node);
+		ite.next(loop);
+	});
+};
 
 const cases = {
-  'simple': ['a', 'b', 'c'],
-  'mixed depth from root': ['a/a', 'a/b', 'a/c', 'b', 'c'],
-  '3 paths deep': ['a', 'a/a', 'a/b', 'a/c', 'a/a/a', 'a/a/b', 'a/a/c']
-}
+	simple: [{ key: "a", schemaKey }, { key: "b", schemaKey }, { key: "c", schemaKey }],
+	"mixed depth from root": [
+		{ key: "a/a", schemaKey: schemaKey2 },
+		{ key: "a/b", schemaKey: schemaKey2 },
+		{ key: "a/c", schemaKey: schemaKey2 },
+		{ key: "b", schemaKey },
+		{ key: "c", schemaKey }
+	],
+	"3 paths deep": [
+		{ key: "a", schemaKey },
+		{ key: "a/a", schemaKey: schemaKey2 },
+		{ key: "a/b", schemaKey: schemaKey2 },
+		{ key: "a/c", schemaKey: schemaKey2 },
+		{ key: "a/a/a", schemaKey: schemaKey3 },
+		{ key: "a/a/b", schemaKey: schemaKey3 },
+		{ key: "a/a/c", schemaKey: schemaKey3 }
+	]
+};
 
 Object.keys(cases).forEach((key) => {
-  tape('iterator is hash order sorted (' + key + ')', function (t) {
-    var keysToTest = cases[key]
-    run(
-      cb => testSingleFeedWithKeys(t, keysToTest, cb),
-      cb => testTwoFeedsWithKeys(t, keysToTest, cb),
-      cb => testSingleFeedWithKeys(t, keysToTest, { reverse: true, sort: reverseSortByHash }, cb),
-      cb => testTwoFeedsWithKeys(t, keysToTest, { reverse: true, sort: reverseSortByHash }, cb),
-      cb => t.end()
-    )
-  })
-})
+	tape("iterator is hash order sorted (" + key + ")", (t) => {
+		const keysToTest = cases[key];
+		run(
+			(cb) => testSingleFeedWithKeys(t, keysToTest, cb),
+			(cb) => testTwoFeedsWithKeys(t, keysToTest, cb),
+			(cb) => testSingleFeedWithKeys(t, keysToTest, { reverse: true, sort: reverseSortByHash }, cb),
+			(cb) => testTwoFeedsWithKeys(t, keysToTest, { reverse: true, sort: reverseSortByHash }, cb),
+			(cb) => t.end()
+		);
+	});
+});
 
-tape('fully visit a folder before visiting the next one', function (t) {
-  t.plan(12)
-  var db = create.one()
-  put(db, ['a', 'a/b', 'a/b/c', 'b/c', 'b/c/d'], function (err) {
-    t.error(err, 'no error')
-    var ite = db.iterator()
+tape("fully visit a folder before visiting the next one", (t) => {
+	t.plan(15);
+	const db = create.one();
+	const batchList = [
+		{
+			type: "add-schema",
+			key: schemaKey,
+			value: schemaValue,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey, schemaValue)),
+			writerAddress: writerAddress
+		},
+		{
+			type: "add-schema",
+			key: schemaKey2,
+			value: schemaValue2,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey2, schemaValue2)),
+			writerAddress: writerAddress
+		},
+		{
+			type: "add-schema",
+			key: schemaKey3,
+			value: schemaValue3,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey3, schemaValue3)),
+			writerAddress: writerAddress
+		}
+	];
 
-    ite.next(function loop (err, val) {
-      t.error(err, 'no error')
-      if (!val) return t.end()
+	db.batch(batchList, (err) => {
+		t.error(err, "no error");
+		put(
+			db,
+			privateKey,
+			writerAddress,
+			[
+				{ key: "a", schemaKey },
+				{ key: "a/b", schemaKey: schemaKey2 },
+				{ key: "a/b/c", schemaKey: schemaKey3 },
+				{ key: "b/c", schemaKey: schemaKey2 },
+				{ key: "b/c/d", schemaKey: schemaKey3 }
+			],
+			{},
+			(err) => {
+				t.error(err, "no error");
+				const ite = db.iterator();
 
-      if (val.key[0] === 'b') {
-        t.same(val.key, 'b/c')
-        ite.next(function (err, val) {
-          t.error(err, 'no error')
-          t.same(val.key, 'b/c/d')
-          ite.next(loop)
-        })
-      } else {
-        t.same(val.key, 'a')
-        ite.next(function (err, val) {
-          t.error(err, 'no error')
-          t.same(val.key, 'a/b')
-          ite.next(function (err, val) {
-            t.error(err, 'no error')
-            t.same(val.key, 'a/b/c')
-            ite.next(loop)
-          })
-        })
-      }
-    })
-  })
-})
-
-function testSingleFeedWithKeys (t, keys, opts, cb) {
-  if (typeof opts === 'function') return testSingleFeedWithKeys(t, keys, null, opts)
-  opts = opts || {}
-
-  var sortFunc = opts.sort || sortByHash
-
-  t.comment('with single feed')
-  var db = create.one()
-  put(db, keys, function (err) {
-    t.error(err, 'no error')
-    testIteratorOrder(t, db.iterator(opts), keys, sortFunc, cb)
-  })
-}
-
-function testTwoFeedsWithKeys (t, keys, opts, cb) {
-  if (typeof opts === 'function') return testTwoFeedsWithKeys(t, keys, null, opts)
-  opts = opts || {}
-
-  var sortFunc = opts.sort || sortByHash
-
-  t.comment('with values split across two feeds')
-  create.two(function (db1, db2, replicate) {
-    var half = Math.floor(keys.length / 2)
-    run(
-      cb => put(db1, keys.slice(0, half), cb),
-      cb => put(db2, keys.slice(half), cb),
-      cb => replicate(cb),
-      cb => testIteratorOrder(t, db1.iterator(opts), keys, sortFunc, cb),
-      cb => testIteratorOrder(t, db2.iterator(opts), keys, sortFunc, cb),
-      done
-    )
-  })
-  function done () {
-    if (!cb) t.end()
-    else cb()
-  }
-}
-
-function testIteratorOrder (t, iterator, expected, sortFunc, done) {
-  var sorted = expected.slice(0).sort(sortFunc)
-  each(iterator, onEach, onDone)
-  function onEach (err, node) {
-    t.error(err, 'no error')
-    var key = node.key || node[0].key
-    t.same(key, sorted.shift())
-  }
-  function onDone () {
-    t.same(sorted.length, 0)
-    if (done === undefined) t.end()
-    else done()
-  }
-}
-
-function each (ite, cb, done) {
-  ite.next(function loop (err, node) {
-    if (err) return cb(err)
-    if (!node) return done()
-    cb(null, node)
-    ite.next(loop)
-  })
-}
+				ite.next(function loop(err, val) {
+					t.error(err, "no error");
+					if (!val) return t.end();
+					if (val.isSchema) {
+						ite.next((err, val) => {
+							ite.next(loop);
+						});
+					}
+					if (val.key[0] === "b") {
+						t.same(val.key, "b/c");
+						ite.next((err, val) => {
+							t.error(err, "no error");
+							t.same(val.key, "b/c/d");
+							ite.next(loop);
+						});
+					} else if (val.key[0] === "a") {
+						t.same(val.key, "a");
+						ite.next((err, val) => {
+							t.error(err, "no error");
+							t.same(val.key, "a/b");
+							ite.next((err, val) => {
+								t.error(err, "no error");
+								t.same(val.key, "a/b/c");
+								ite.next(loop);
+							});
+						});
+					}
+				});
+			}
+		);
+	});
+});
