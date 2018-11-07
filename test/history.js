a34d8a7b4c655f16a9eae6a55716c5feeeae5f9f
Johan <johan@paramation.com>
2018-11-07 15:10:28 +0700

    Fix test/history.js

--- test/history.js
+++ test/history.js
@@ -1,130 +1,227 @@
-var tape = require('tape')
-var collect = require('stream-collector')
-var create = require('./helpers/create')
-var replicate = require('./helpers/replicate')
+const tape = require("tape");
+const collect = require("stream-collector");
+const create = require("./helpers/create");
+const replicate = require("./helpers/replicate");
+const EthCrypto = require("eth-crypto");
+const { privateKey, publicKey: writerAddress } = EthCrypto.createIdentity();
 
-tape('empty history', function (t) {
-  var db = create.one()
-  var expected = []
+tape("empty history", (t) => {
+	const db = create.one();
+	const expected = [];
 
-  var rs = db.createHistoryStream()
-  collect(rs, function (err, actual) {
-    t.error(err, 'no error')
-    t.deepEqual(actual, expected, 'diff as expected')
-    t.end()
-  })
-})
+	const rs = db.createHistoryStream();
+	collect(rs, (err, actual) => {
+		t.error(err, "no error");
+		t.deepEqual(actual, expected, "diff as expected");
+		t.end();
+	});
+});
 
-tape('single value', function (t) {
-  var db = create.one()
+tape("single value", (t) => {
+	const db = create.one();
+	const schemaKey = "schema/*";
+	const schemaValue = {
+		keySchema: "*",
+		valueValidationKey: "",
+		keyValidation: ""
+	};
+	db.addSchema(schemaKey, schemaValue, EthCrypto.sign(privateKey, db.createSignHash(schemaKey, schemaValue)), writerAddress, (err) => {
+		t.error(err, "no error");
 
-  db.put('a', '2', function (err) {
-    t.error(err, 'no error')
-    var rs = db.createHistoryStream()
-    collect(rs, function (err, actual) {
-      t.error(err, 'no error')
-      t.equals(actual.length, 1)
-      t.equals(actual[0].key, 'a')
-      t.equals(actual[0].value, '2')
-      t.end()
-    })
-  })
-})
+		db.put("a", "2", EthCrypto.sign(privateKey, db.createSignHash("a", "2")), writerAddress, { schemaKey }, (err) => {
+			t.error(err, "no error");
+			const rs = db.createHistoryStream();
+			collect(rs, (err, actual) => {
+				t.error(err, "no error");
+				t.equals(actual.length, 2);
+				t.equals(actual[1].key, "a");
+				t.equals(actual[1].value, "2");
+				t.end();
+			});
+		});
+	});
+});
 
-tape('multiple values', function (t) {
-  var db = create.one()
+tape("multiple values", (t) => {
+	const db = create.one();
+	const schemaKey = "schema/*";
+	const schemaValue = {
+		keySchema: "*",
+		valueValidationKey: "",
+		keyValidation: ""
+	};
 
-  db.put('a', '2', function (err) {
-    t.error(err, 'no error')
-    db.put('b/0', 'boop', function (err) {
-      t.error(err, 'no error')
-      var rs = db.createHistoryStream()
-      collect(rs, function (err, actual) {
-        t.error(err, 'no error')
-        t.equals(actual.length, 2)
-        t.equals(actual[0].key, 'a')
-        t.equals(actual[0].value, '2')
-        t.equals(actual[1].key, 'b/0')
-        t.equals(actual[1].value, 'boop')
-        t.end()
-      })
-    })
-  })
-})
+	const schemaKey2 = "schema/*/*";
+	const schemaValue2 = {
+		keySchema: "*/*",
+		valueValidationKey: "",
+		keyValidation: ""
+	};
 
-tape('multiple values: same key', function (t) {
-  var db = create.one()
+	const batchList = [
+		{
+			type: "add-schema",
+			key: schemaKey,
+			value: schemaValue,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey, schemaValue)),
+			writerAddress: writerAddress
+		},
+		{
+			type: "add-schema",
+			key: schemaKey2,
+			value: schemaValue2,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey2, schemaValue2)),
+			writerAddress: writerAddress
+		}
+	];
 
-  db.put('a', '2', function (err) {
-    t.error(err, 'no error')
-    db.put('a', 'boop', function (err) {
-      t.error(err, 'no error')
-      var rs = db.createHistoryStream()
-      collect(rs, function (err, actual) {
-        t.error(err, 'no error')
-        t.equals(actual.length, 2)
-        t.equals(actual[0].key, 'a')
-        t.equals(actual[0].value, '2')
-        t.equals(actual[1].key, 'a')
-        t.equals(actual[1].value, 'boop')
-        t.end()
-      })
-    })
-  })
-})
+	db.batch(batchList, (err) => {
+		t.error(err, "no error");
+		db.put("a", "2", EthCrypto.sign(privateKey, db.createSignHash("a", "2")), writerAddress, { schemaKey }, (err) => {
+			t.error(err, "no error");
+			db.put(
+				"b/0",
+				"boop",
+				EthCrypto.sign(privateKey, db.createSignHash("b/0", "boop")),
+				writerAddress,
+				{ schemaKey: schemaKey2 },
+				(err) => {
+					t.error(err, "no error");
+					const rs = db.createHistoryStream();
+					collect(rs, (err, actual) => {
+						t.error(err, "no error");
+						t.equals(actual.length, 4);
+						t.equals(actual[2].key, "a");
+						t.equals(actual[2].value, "2");
+						t.equals(actual[3].key, "b/0");
+						t.equals(actual[3].value, "boop");
+						t.end();
+					});
+				}
+			);
+		});
+	});
+});
 
-tape('2 feeds', function (t) {
-  create.two(function (a, b) {
-    a.put('a', 'a', function () {
-      b.put('b', '12', function () {
-        replicate(a, b, validate)
-      })
-    })
+tape("multiple values: same key", (t) => {
+	const db = create.one();
+	const schemaKey = "schema/*";
+	const schemaValue = {
+		keySchema: "*",
+		valueValidationKey: "",
+		keyValidation: ""
+	};
+	db.addSchema(schemaKey, schemaValue, EthCrypto.sign(privateKey, db.createSignHash(schemaKey, schemaValue)), writerAddress, (err) => {
+		t.error(err, "no error");
+		db.put("a", "2", EthCrypto.sign(privateKey, db.createSignHash("a", "2")), writerAddress, { schemaKey }, (err) => {
+			t.error(err, "no error");
+			db.put("a", "boop", EthCrypto.sign(privateKey, db.createSignHash("a", "boop")), writerAddress, { schemaKey }, (err) => {
+				t.error(err, "no error");
+				const rs = db.createHistoryStream();
+				collect(rs, (err, actual) => {
+					t.error(err, "no error");
+					t.equals(actual.length, 3);
+					t.equals(actual[1].key, "a");
+					t.equals(actual[1].value, "2");
+					t.equals(actual[2].key, "a");
+					t.equals(actual[2].value, "boop");
+					t.end();
+				});
+			});
+		});
+	});
+});
 
-    function validate () {
-      var rs = b.createHistoryStream()
-      var bi = b.feeds.indexOf(b.local)
-      var ai = bi === 0 ? 1 : 0
+tape("2 feeds", (t) => {
+	create.two((a, b) => {
+		const schemaKey = "schema/*";
+		const schemaValue = {
+			keySchema: "*",
+			valueValidationKey: "",
+			keyValidation: ""
+		};
 
-      collect(rs, function (err, actual) {
-        t.error(err, 'no error')
-        t.equals(actual.length, 3)
-        t.equals(actual[0].feed, ai)
-        t.equals(actual[0].seq, 1)
-        t.equals(actual[1].feed, ai)
-        t.equals(actual[1].seq, 2)
-        t.equals(actual[2].feed, bi)
-        t.equals(actual[2].seq, 1)
-        t.end()
-      })
-    }
-  })
-})
+		const validate = () => {
+			const rs = b.createHistoryStream();
+			const bi = b.feeds.indexOf(b.local);
+			const ai = bi === 0 ? 1 : 0;
 
-tape('reverse', function (t) {
-  create.two(function (a, b) {
-    a.put('a', 'a', function () {
-      b.put('b', '12', function () {
-        replicate(a, b, validate)
-      })
-    })
+			collect(rs, (err, actual) => {
+				t.error(err, "no error");
+				t.equals(actual.length, 5);
+				t.equals(actual[0].feed, ai);
+				t.equals(actual[0].seq, 1);
+				t.equals(actual[2].feed, ai);
+				t.equals(actual[2].seq, 3);
+				t.equals(actual[4].feed, bi);
+				t.equals(actual[4].seq, 2);
+				t.end();
+			});
+		};
 
-    function validate () {
-      var rs = b.createHistoryStream({reverse: true})
-      var bi = b.feeds.indexOf(b.local)
-      var ai = bi === 0 ? 1 : 0
+		a.addSchema(schemaKey, schemaValue, EthCrypto.sign(privateKey, a.createSignHash(schemaKey, schemaValue)), writerAddress, (err) => {
+			t.error(err, "no error");
+			b.addSchema(
+				schemaKey,
+				schemaValue,
+				EthCrypto.sign(privateKey, b.createSignHash(schemaKey, schemaValue)),
+				writerAddress,
+				(err) => {
+					t.error(err, "no error");
+					a.put("a", "a", EthCrypto.sign(privateKey, a.createSignHash("a", "a")), writerAddress, { schemaKey }, (err) => {
+						b.put("b", "12", EthCrypto.sign(privateKey, b.createSignHash("b", "12")), writerAddress, { schemaKey }, (err) => {
+							replicate(a, b, validate);
+						});
+					});
+				}
+			);
+		});
+	});
+});
 
-      collect(rs, function (err, actual) {
-        t.error(err, 'no error')
-        t.equals(actual.length, 3)
-        t.equals(actual[0].feed, bi)
-        t.equals(actual[0].seq, 1)
-        t.equals(actual[1].feed, ai)
-        t.equals(actual[1].seq, 2)
-        t.equals(actual[2].feed, ai)
-        t.equals(actual[2].seq, 1)
-        t.end()
-      })
-    }
-  })
-})
+tape("reverse", (t) => {
+	create.two((a, b) => {
+		const schemaKey = "schema/*";
+		const schemaValue = {
+			keySchema: "*",
+			valueValidationKey: "",
+			keyValidation: ""
+		};
+
+		const validate = () => {
+			const rs = b.createHistoryStream({ reverse: true });
+			const bi = b.feeds.indexOf(b.local);
+			const ai = bi === 0 ? 1 : 0;
+
+			collect(rs, (err, actual) => {
+				t.error(err, "no error");
+				t.equals(actual.length, 5);
+				t.equals(actual[0].feed, bi);
+				t.equals(actual[0].seq, 2);
+				t.equals(actual[2].feed, ai);
+				t.equals(actual[2].seq, 3);
+				t.equals(actual[4].feed, ai);
+				t.equals(actual[4].seq, 1);
+				t.end();
+			});
+		};
+
+		a.addSchema(schemaKey, schemaValue, EthCrypto.sign(privateKey, a.createSignHash(schemaKey, schemaValue)), writerAddress, (err) => {
+			t.error(err, "no error");
+			b.addSchema(
+				schemaKey,
+				schemaValue,
+				EthCrypto.sign(privateKey, b.createSignHash(schemaKey, schemaValue)),
+				writerAddress,
+				(err) => {
+					t.error(err, "no error");
+					a.put("a", "a", EthCrypto.sign(privateKey, a.createSignHash("a", "a")), writerAddress, { schemaKey }, (err) => {
+						b.put("b", "12", EthCrypto.sign(privateKey, b.createSignHash("b", "12")), writerAddress, { schemaKey }, (err) => {
+							replicate(a, b, validate);
+						});
+					});
+				}
+			);
+		});
+	});
+});
