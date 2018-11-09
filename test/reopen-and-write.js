7ecff864fe9c38521f67490d5a2bc3b2d056257f
Johan <johan@paramation.com>
2018-11-09 13:25:12 +0700

    Fix test/reopen-and-write.js

--- test/reopen-and-write.js
+++ test/reopen-and-write.js
@@ -1,74 +1,121 @@
-var tape = require('tape')
-var create = require('./helpers/create')
-var run = require('./helpers/run')
-var hyperdb = require('..')
-var messages = require('../lib/messages')
+const tape = require("tape");
+const create = require("./helpers/create");
+const run = require("./helpers/run");
+const aodb = require("..");
+const messages = require("../lib/messages");
+const EthCrypto = require("eth-crypto");
+const { privateKey, publicKey: writerAddress } = EthCrypto.createIdentity();
 
-tape('3 writers, re-open and write, re-open again', function (t) {
-  create.three(function (a, b, c) {
-    var reopened
+const schemaKey = "schema/*";
+const schemaValue = {
+	keySchema: "*",
+	valueValidationKey: "",
+	keyValidation: ""
+};
 
-    run(
-      cb => a.put('foo', 'bar', cb),
-      testUncorrupted,
-      reopenDb,
-      cb => reopened.put('foo2', 'bar2', cb),
-      reopenDb,
-      testInflateValue,
-      done
-    )
+const reuseStorage = (db) => {
+	return function(name) {
+		let match = name.match(/^source\/(.*)/);
+		if (match) {
+			name = match[1];
+			if (name === "secret_key") return db.source._storage.secretKey;
+			return db.source._storage[name];
+		}
+		match = name.match(/^peers\/([0-9a-f]+)\/(.*)/);
+		if (match) {
+			const hex = match[1];
+			name = match[2];
+			const peerWriter = db._writers.find((writer) => {
+				return writer && writer._feed.discoveryKey.toString("hex") === hex;
+			});
+			if (!peerWriter) throw new Error("mismatch");
+			const feed = peerWriter._feed;
+			if (name === "secret_key") return feed._storage.secretKey;
+			return feed._storage[name];
+		} else {
+			throw new Error("mismatch");
+		}
+	};
+};
 
-    function done (err) {
-      t.error(err, 'no error')
-      t.end()
-    }
+tape("3 writers, re-open and write, re-open again", (t) => {
+	create.three((a, b, c) => {
+		a.addSchema(schemaKey, schemaValue, EthCrypto.sign(privateKey, a.createSignHash(schemaKey, schemaValue)), writerAddress, (err) => {
+			t.error(err, "no error");
+			b.addSchema(
+				schemaKey,
+				schemaValue,
+				EthCrypto.sign(privateKey, b.createSignHash(schemaKey, schemaValue)),
+				writerAddress,
+				(err) => {
+					t.error(err, "no error");
+					c.addSchema(
+						schemaKey,
+						schemaValue,
+						EthCrypto.sign(privateKey, c.createSignHash(schemaKey, schemaValue)),
+						writerAddress,
+						(err) => {
+							t.error(err, "no error");
 
-    function testUncorrupted (cb) {
-      t.equal(a._writers.length, 3, 'correct number of writers')
-      cb()
-    }
+							let reopened;
 
-    function reopenDb (cb) {
-      reopened = hyperdb(reuseStorage(a))
-      reopened.ready(function (err) {
-        t.error(err, 'no error')
-        cb()
-      })
-    }
+							const done = (err) => {
+								t.error(err, "no error");
+								t.end();
+							};
 
-    function testInflateValue (cb) {
-      t.equals(reopened.source.length, 5, 'correct length')
-      reopened.source.get(4, function (err, data) {
-        t.error(err, 'no error')
-        var val = messages.Entry.decode(data)
-        t.equal(val.inflate, 2, 'correct inflate for new entry')
-        cb()
-      })
-    }
-  })
-})
+							const testUncorrupted = (cb) => {
+								t.equal(a._writers.length, 3, "correct number of writers");
+								cb();
+							};
 
-function reuseStorage (db) {
-  return function (name) {
-    var match = name.match(/^source\/(.*)/)
-    if (match) {
-      name = match[1]
-      if (name === 'secret_key') return db.source._storage.secretKey
-      return db.source._storage[name]
-    }
-    match = name.match(/^peers\/([0-9a-f]+)\/(.*)/)
-    if (match) {
-      var hex = match[1]
-      name = match[2]
-      var peerWriter = db._writers.find(function (writer) {
-        return writer && writer._feed.discoveryKey.toString('hex') === hex
-      })
-      if (!peerWriter) throw new Error('mismatch')
-      var feed = peerWriter._feed
-      if (name === 'secret_key') return feed._storage.secretKey
-      return feed._storage[name]
-    } else {
-      throw new Error('mismatch')
-    }
-  }
-}
+							const reopenDb = (cb) => {
+								reopened = aodb(reuseStorage(a), { valueEncoding: "json" });
+								reopened.ready((err) => {
+									t.error(err, "no error");
+									cb();
+								});
+							};
+
+							const testInflateValue = (cb) => {
+								t.equals(reopened.source.length, 6, "correct length");
+								reopened.source.get(4, (err, data) => {
+									t.error(err, "no error");
+									var val = messages.Entry.decode(data);
+									t.equal(val.inflate, 2, "correct inflate for new entry");
+									cb();
+								});
+							};
+
+							run(
+								(cb) =>
+									a.put(
+										"foo",
+										"bar",
+										EthCrypto.sign(privateKey, a.createSignHash("foo", "bar")),
+										writerAddress,
+										{ schemaKey },
+										cb
+									),
+								testUncorrupted,
+								reopenDb,
+								(cb) =>
+									reopened.put(
+										"foo2",
+										"bar2",
+										EthCrypto.sign(privateKey, reopened.createSignHash("foo2", "bar2")),
+										writerAddress,
+										{ schemaKey },
+										cb
+									),
+								reopenDb,
+								testInflateValue,
+								done
+							);
+						}
+					);
+				}
+			);
+		});
+	});
+});
