64a68fdd0b93fcf11ef6c6d8df323cb98d0cf8ab
Johan <johan@paramation.com>
2018-11-09 16:10:57 +0700

    Fix test/watch.js

--- test/watch.js
+++ test/watch.js
@@ -1,110 +1,208 @@
-var tape = require('tape')
-var create = require('./helpers/create')
-var replicate = require('./helpers/replicate')
-var run = require('./helpers/run')
+const tape = require("tape");
+const create = require("./helpers/create");
+const replicate = require("./helpers/replicate");
+const run = require("./helpers/run");
+const EthCrypto = require("eth-crypto");
+const { privateKey, publicKey: writerAddress } = EthCrypto.createIdentity();
 
-tape('basic watch', function (t) {
-  var db = create.one()
+const schemaKey = "schema/*";
+const schemaValue = {
+	keySchema: "*",
+	valueValidationKey: "",
+	keyValidation: ""
+};
 
-  db.watch(function () {
-    t.pass('watch triggered')
-    t.end()
-  })
+const schemaKey2 = "schema/*/*";
+const schemaValue2 = {
+	keySchema: "*/*",
+	valueValidationKey: "",
+	keyValidation: ""
+};
 
-  db.put('hello', 'world')
-})
+const schemaKey3 = "schema/*/*/*";
+const schemaValue3 = {
+	keySchema: "*/*/*",
+	valueValidationKey: "",
+	keyValidation: ""
+};
 
-tape('watch prefix', function (t) {
-  var db = create.one()
-  var changed = false
+tape("basic watch", (t) => {
+	const db = create.one();
 
-  db.watch('foo', function () {
-    t.ok(changed)
-    t.end()
-  })
+	db.addSchema(schemaKey, schemaValue, EthCrypto.sign(privateKey, db.createSignHash(schemaKey, schemaValue)), writerAddress, (err) => {
+		t.error(err, "no error");
 
-  db.put('hello', 'world', function (err) {
-    t.error(err)
-    setImmediate(function () {
-      changed = true
-      db.put('foo/bar', 'baz')
-    })
-  })
-})
+		db.watch(() => {
+			t.pass("watch triggered");
+			t.end();
+		});
 
-tape('recursive watch', function (t) {
-  t.plan(20)
+		db.put("hello", "world", EthCrypto.sign(privateKey, db.createSignHash("hello", "world")), writerAddress, { schemaKey });
+	});
+});
 
-  var i = 0
-  var db = create.one()
+tape("watch prefix", (t) => {
+	const db = create.one();
+	let changed = false;
+	db.addSchema(schemaKey, schemaValue, EthCrypto.sign(privateKey, db.createSignHash(schemaKey, schemaValue)), writerAddress, (err) => {
+		t.error(err, "no error");
 
-  db.watch('foo', function () {
-    if (i === 20) return
-    t.pass('watch triggered')
-    db.put('foo', 'bar-' + (++i))
-  })
+		db.watch("foo", () => {
+			t.ok(changed);
+			t.end();
+		});
 
-  db.put('foo', 'bar')
-})
+		db.put("hello", "world", EthCrypto.sign(privateKey, db.createSignHash("hello", "world")), writerAddress, { schemaKey }, (err) => {
+			t.error(err);
+			setImmediate(() => {
+				changed = true;
+				db.put("foo/bar", "baz", EthCrypto.sign(privateKey, db.createSignHash("foo/bar", "baz")), writerAddress, { schemaKey });
+			});
+		});
+	});
+});
 
-tape('watch and stop watching', function (t) {
-  var db = create.one()
-  var once = true
+tape("recursive watch", (t) => {
+	t.plan(21);
 
-  var w = db.watch('foo', function () {
-    t.ok(once)
-    once = false
-    w.destroy()
-    db.put('foo/bar/baz', 'qux', function () {
-      t.end()
-    })
-  })
+	let i = 0;
+	const db = create.one();
 
-  db.put('foo/bar', 'baz')
-})
+	db.addSchema(schemaKey, schemaValue, EthCrypto.sign(privateKey, db.createSignHash(schemaKey, schemaValue)), writerAddress, (err) => {
+		t.error(err, "no error");
+		db.watch("foo", () => {
+			if (i === 20) return;
+			t.pass("watch triggered");
+			++i;
+			db.put("foo", "bar-" + i, EthCrypto.sign(privateKey, db.createSignHash("foo", "bar-" + i)), writerAddress, { schemaKey });
+		});
 
-tape('remote watch', function (t) {
-  var db = create.one()
+		db.put("foo", "bar", EthCrypto.sign(privateKey, db.createSignHash("foo", "bar")), writerAddress, { schemaKey });
+	});
+});
 
-  db.ready(function () {
-    var clone = create.one(db.key)
+tape("watch and stop watching", (t) => {
+	const db = create.one();
+	const batchList = [
+		{
+			type: "add-schema",
+			key: schemaKey2,
+			value: schemaValue2,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey2, schemaValue2)),
+			writerAddress: writerAddress
+		},
+		{
+			type: "add-schema",
+			key: schemaKey3,
+			value: schemaValue3,
+			writerSignature: EthCrypto.sign(privateKey, db.createSignHash(schemaKey3, schemaValue3)),
+			writerAddress: writerAddress
+		}
+	];
+	db.batch(batchList, (err) => {
+		t.error(err, "no error");
 
-    for (var i = 0; i < 100; i++) db.put('hello-' + i, 'world-' + i)
-    db.put('flush', 'flush', function () {
-      clone.watch(function () {
-        t.pass('remote watch triggered')
-        t.end()
-      })
+		let once = true;
 
-      replicate(db, clone)
-    })
-  })
-})
+		const w = db.watch("foo", () => {
+			t.ok(once);
+			once = false;
+			w.destroy();
+			db.put(
+				"foo/bar/baz",
+				"qux",
+				EthCrypto.sign(privateKey, db.createSignHash("foo/bar/baz", "qux")),
+				writerAddress,
+				{ schemaKey: schemaKey3 },
+				() => {
+					t.end();
+				}
+			);
+		});
 
-tape('watch with 3rd-party authorize', function (t) {
-  create.two(function (a, b) {
-    t.plan(3) // once per writer updated in the namespace (b.auth and c.put) and .error
+		db.put("foo/bar", "baz", EthCrypto.sign(privateKey, db.createSignHash("foo/bar", "baz")), writerAddress, { schemaKey: schemaKey2 });
+	});
+});
 
-    a.watch(function () {
-      t.pass('watch called')
-    })
+tape("remote watch", (t) => {
+	const db = create.one();
 
-    var c = create.one(a.key)
-    c.ready(function () {
-      run(
-        cb => replicate(a, b, cb),
-        cb => replicate(b, c, cb),
-        cb => b.authorize(c.local.key, cb),
-        cb => replicate(b, c, cb),
-        cb => c.put('hello2', 'world2', cb),
-        cb => replicate(b, c, cb),
-        cb => replicate(a, b, cb),
-        done
-      )
+	db.ready(() => {
+		db.addSchema(
+			schemaKey,
+			schemaValue,
+			EthCrypto.sign(privateKey, db.createSignHash(schemaKey, schemaValue)),
+			writerAddress,
+			(err) => {
+				t.error(err, "no error");
 
-      function done (err) {
-        t.error(err, 'no error')
-      }
-    })
-  })
-})
+				const clone = create.one(db.key);
+
+				for (let i = 0; i < 100; i++)
+					db.put(
+						"hello-" + i,
+						"world-" + i,
+						EthCrypto.sign(privateKey, db.createSignHash("hello-" + i, "world-" + i)),
+						writerAddress,
+						{ schemaKey }
+					);
+
+				db.put(
+					"flush",
+					"flush",
+					EthCrypto.sign(privateKey, db.createSignHash("flush", "flush")),
+					writerAddress,
+					{ schemaKey },
+					() => {
+						clone.watch(() => {
+							t.pass("remote watch triggered");
+							t.end();
+						});
+
+						replicate(db, clone);
+					}
+				);
+			}
+		);
+	});
+});
+
+tape("watch with 3rd-party authorize", (t) => {
+	create.two((a, b) => {
+		t.plan(4); // once per writer updated in the namespace (b.auth and c.put) and .error
+
+		a.addSchema(schemaKey, schemaValue, EthCrypto.sign(privateKey, a.createSignHash(schemaKey, schemaValue)), writerAddress, (err) => {
+			t.error(err, "no error");
+
+			a.watch(() => {
+				t.pass("watch called");
+			});
+
+			const c = create.one(a.key);
+			c.ready(() => {
+				const done = (err) => {
+					t.error(err, "no error");
+				};
+
+				run(
+					(cb) => replicate(a, b, cb),
+					(cb) => replicate(b, c, cb),
+					(cb) => b.authorize(c.local.key, cb),
+					(cb) => replicate(b, c, cb),
+					(cb) =>
+						c.put(
+							"hello2",
+							"world2",
+							EthCrypto.sign(privateKey, c.createSignHash("hello2", "world2")),
+							writerAddress,
+							{ schemaKey },
+							cb
+						),
+					(cb) => replicate(b, c, cb),
+					(cb) => replicate(a, b, cb),
+					done
+				);
+			});
+		});
+	});
+});
